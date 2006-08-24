4fc83a0eb2ae7734413f9ebcca963c20d2c6ae9c
carlmanaster <carlmanaster@53f6efc0-b81b-0410-b9fd-bdae7ed662e3>
2006-08-24 04:52:08 +0000

    created Scale to translate between screen and data coordinates (1D)

---
+++ trunk/Allelogram/org/carlmanaster/allelogram/gui/Scale.java
@@ -0,0 +1,21 @@
+package org.carlmanaster.allelogram.gui;
+
+public class Scale {
+	private final double xLo;
+	private final double xHi;
+	private final double nLo;
+	private final double nHi;
+
+	public Scale(double xLo, double xHi, int nLo, int nHi) {
+		this.xLo = xLo;
+		this.xHi = xHi;
+		this.nLo = nLo;
+		this.nHi = nHi;
+	}
+
+	public int toScreen(double x)	{return (int) (nLo + ((x - xLo) / xWidth()) * nWidth());}
+	public double toData(int n)	{return xLo + ((n - nLo) / nWidth()) * xWidth();}
+
+	private double nWidth() {return nHi - nLo;}
+	private double xWidth() {return xHi - xLo;}
+}
--- trunk/Allelogram/org/carlmanaster/allelogram/gui/tests/ScaleTest.java
+++ trunk/Allelogram/org/carlmanaster/allelogram/gui/tests/ScaleTest.java
@@ -2,9 +2,33 @@ package org.carlmanaster.allelogram.gui.tests;
 
 import junit.framework.TestCase;
 
+import org.carlmanaster.allelogram.gui.Scale;
+
 public class ScaleTest extends TestCase {
-	public void testZzz() throws Exception {
-		assertTrue(true);
+
+	private Scale scale;
+
+	protected void setUp() throws Exception {
+		scale = new Scale(1.0, 11.0, 0, 1000);
+	}
+	
+	public void testToScreen() throws Exception {
+		assertEquals(0, scale.toScreen(1.0));
+		assertEquals(50, scale.toScreen(1.5));
+		assertEquals(100, scale.toScreen(2.0));
+	}
+
+	public void testToData() throws Exception {
+		assertEquals(1.0, scale.toData(0));
+		assertEquals(1.5, scale.toData(50));
+		assertEquals(2.0, scale.toData(100));
+	}
+	
+	public void testMany() throws Exception {
+		for (int i = 0; i < 1000; ++i) {
+			double x = scale.toData(i);
+			assertEquals(x, scale.toData(scale.toScreen(x)), 0.02);
+		}
 	}
 
 }
