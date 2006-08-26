17486ae329db4d30ee317c14e8ce9f2948a3e490
carlmanaster <carlmanaster@53f6efc0-b81b-0410-b9fd-bdae7ed662e3>
2006-08-26 18:24:29 +0000

    added BinGuesser

--- Allelogram/org/carlmanaster/allelogram/model/Bin.java
+++ Allelogram/org/carlmanaster/allelogram/model/Bin.java
@@ -1,8 +1,8 @@
 package org.carlmanaster.allelogram.model;
 
 public class Bin {
-	private final double lo;
-	private final double hi;
+	private final Double lo;
+	private final Double hi;
 
 	public Bin(double lo, double hi) {
 		this.lo = lo;
@@ -18,5 +18,25 @@ public class Bin {
 	public boolean contains(Allele allele) {
 		return contains(allele.getAdjustedValue());
 	}
+	
+	public boolean equals(Object obj) {
+		if (obj == null)				return false;
+		if (obj == this)				return true;
+		if (!(obj instanceof Bin))	return false;
+		Bin that = (Bin) obj;
+		if (!this.lo.equals(that.lo))	return false;
+		if (!this.hi.equals(that.hi))	return false;
+		return true;
+	}
+	
+	public int hashCode() {
+		int hash = 1;
+		hash = hash * 31 + lo.hashCode();
+		hash = hash * 31 + hi.hashCode();
+		return hash;
+	}
 
+	public String toString() {
+		return String.format("(%1.1f, %1.1f)", lo, hi);
+	}
 }
