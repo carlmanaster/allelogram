166ae46ad55575ff0cdcf9cd42f93b5400463e57
carlmanaster <carlmanaster@53f6efc0-b81b-0410-b9fd-bdae7ed662e3>
2006-08-29 04:49:01 +0000

    progress on Settings, however there is a lot still to do.  Notably, it's not detecting (nor storing) the delimiter for classifications.  Probably the next step should be to go back to Classification and retrofit delimiter(s). Also, constructing from String should move to Classification from Settings.

--- trunk/Allelogram/org/carlmanaster/allelogram/model/Settings.java
+++ trunk/Allelogram/org/carlmanaster/allelogram/model/Settings.java
@@ -1,12 +1,44 @@
 package org.carlmanaster.allelogram.model;
 
-public class Settings {
+import java.util.ArrayList;
+import java.util.Vector;
 
-    public static String stripComments(String line) {
+public class Settings {
+	final Vector<String> columns = new Vector<String>();
+	private Vector<Classification> classifications = new Vector<Classification>();
+
+    public Settings(String s) {
+    		ArrayList<String> lines = new ArrayList<String>();
+    		for (String line : s.split("\n"))
+    			lines.add(stripComments(line));
+    		
+    		int columnStart = lines.indexOf("columns") + 1;
+    		int classificationStart = lines.indexOf("classifications") + 1;
+    		int sortStart = lines.indexOf("sort") + 1;
+    		
+    		for (int i = columnStart; i < classificationStart - 1; ++i)
+    			columns.add(lines.get(i));
+    		
+    		for (int i = classificationStart; i < sortStart - 1; ++i)
+    			classifications.add(makeClassification(lines.get(i)));
+	}
+
+	private Classification makeClassification(String string) {
+		String[] terms = string.split(":");
+		String name = terms[0];
+		String[] columns = terms[1].split("-");
+		Classification c = new Classification(name, columns);
+		return c;
+	}
+
+	public static String stripComments(String line) {
         int commentStart = line.indexOf('!');
         if (commentStart < 0)
             return line;
         return line.substring(0, commentStart).trim();
     }
+
+	public Vector<String> getColumns()					{return columns;}
+	public Vector<Classification> getClassifications()		{return classifications;}
 
 }
