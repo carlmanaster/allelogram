74feeef9e336d0d5380f18c5f3ead6d4adafd348
carlmanaster <carlmanaster@53f6efc0-b81b-0410-b9fd-bdae7ed662e3>
2006-10-02 12:20:56 +0000

    Made tick marks shorter.

--- Allelogram/org/carlmanaster/allelogram/util/FrameCloser.java
+++ Allelogram/org/carlmanaster/allelogram/util/FrameCloser.java
@@ -1,20 +1,10 @@
 package org.carlmanaster.allelogram.util;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class FrameCloser 
-  extends java.awt.event.WindowAdapter
-{
-  /***********************************************************************/
-
-  public void windowClosing(java.awt.event.WindowEvent e) 
-  {
-		System.exit(0);
-	}
-  /***********************************************************************/
-  
-	public void windowClosed(java.awt.event.WindowEvent e) 
-  {
-    System.exit(0);
-  }	
-  /***********************************************************************/
+public class FrameCloser extends WindowAdapter {
+	public void windowClosing(WindowEvent e)	{exit();}
+	public void windowClosed(WindowEvent e)	{exit();}
+	private void exit()						{System.exit(0);}
 }
--- Allelogram/org/carlmanaster/allelogram/util/PrintUtils.java
+++ Allelogram/org/carlmanaster/allelogram/util/PrintUtils.java
@@ -1,80 +1,56 @@
 package org.carlmanaster.allelogram.util;
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.print.*;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 
-public class PrintUtils implements Printable
-{
-  private Component componentToBePrinted;
-  /***********************************************************************/
-  public static void printComponent(Component c)
-  {
-    printComponent(c, true);
-  }
-  /***********************************************************************/
-  public static void printComponent(Component c, boolean prompt)
-  {
-    new PrintUtils(c).print(prompt);
-  }
-  /***********************************************************************/
-  public PrintUtils(Component componentToBePrinted)
-  {
-    this.componentToBePrinted = componentToBePrinted;
-  }
-  /***********************************************************************/
-  public void print(boolean prompt)
-  {
-    PrinterJob printJob = PrinterJob.getPrinterJob();
-    PageFormat format = printJob.defaultPage();
-    Paper paper = format.getPaper();
-    //paper.setImageableArea(18, 0, 180, 840);// Paper format for receipt printer
-    format.setPaper(paper);
-    printJob.setPrintable(this, format);
-    if (!prompt || printJob.printDialog())
-    {
-      try
-      {
-        printJob.print();
-      }
-      catch (PrinterException pe)
-      {
-        System.out.println("Error printing: " + pe);
-      }
-    }
-  }
-  /***********************************************************************/
-  public int print(Graphics g, PageFormat pageFormat, int pageIndex)
-  {
-    if (pageIndex > 0)
-    {
-      return (NO_SUCH_PAGE);
-    }
-    else
-    {
-      Graphics2D g2d = (Graphics2D) g;
-      //			My_System.variable("width = " + pageFormat.getWidth());
-      //      My_System.variable("image width = " + pageFormat.getImageableWidth());
-      //			My_System.variable("getHeight() = " + pageFormat.getHeight());
-      g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
-      disableDoubleBuffering(componentToBePrinted);
-      componentToBePrinted.paint(g2d);
-      enableDoubleBuffering(componentToBePrinted);
-      return (PAGE_EXISTS);
-    }
-  }
-  /***********************************************************************/
-  public static void disableDoubleBuffering(Component c)
-  {
-    RepaintManager currentManager = RepaintManager.currentManager(c);
-    currentManager.setDoubleBufferingEnabled(false);
-  }
-  /***********************************************************************/
-  public static void enableDoubleBuffering(Component c)
-  {
-    RepaintManager currentManager = RepaintManager.currentManager(c);
-    currentManager.setDoubleBufferingEnabled(true);
-  }
-  /***********************************************************************/
-  /***********************************************************************/
+import javax.swing.RepaintManager;
+
+public class PrintUtils implements Printable {
+	private Component printee;
+
+	public static void printComponent(Component c) {
+		printComponent(c, true);
+	}
+
+	private static void printComponent(Component c, boolean prompt) {
+		new PrintUtils(c).print(prompt);
+	}
+
+	private PrintUtils(Component componentToBePrinted) {
+		this.printee = componentToBePrinted;
+	}
+
+	private void print(boolean prompt) {
+		PrinterJob	printJob	= PrinterJob.getPrinterJob();
+		printJob.setPrintable(this);
+		if (!prompt || printJob.printDialog()) {
+			try {
+				printJob.print();
+			} catch (PrinterException pe) {
+				System.out.println("Error printing: " + pe);
+			}
+		}
+	}
+
+	public int print(Graphics g, PageFormat pageFormat, int pageIndex) {
+		if (pageIndex > 0)
+			return (NO_SUCH_PAGE);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+		disableDoubleBuffering(printee);
+		printee.paint(g2d);
+		enableDoubleBuffering(printee);
+		return (PAGE_EXISTS);
+	}
+	
+	private static void disableDoubleBuffering(Component c)	{setDoubleBuffering(c, false);}
+	private static void enableDoubleBuffering(Component c)	{setDoubleBuffering(c, true);}
+	private static void setDoubleBuffering(Component c, boolean value) {
+		RepaintManager.currentManager(c).setDoubleBufferingEnabled(value);
+	}
 }
