59eb9c8d3c11fd76f40052d37f2e1555833d45c1
carlmanaster <carlmanaster@53f6efc0-b81b-0410-b9fd-bdae7ed662e3>
2007-01-11 03:46:46 +0000

    Color groups in display order

--- trunk/Allelogram/org/carlmanaster/allelogram/gui/Colorizer.java
+++ trunk/Allelogram/org/carlmanaster/allelogram/gui/Colorizer.java
@@ -2,34 +2,37 @@ package org.carlmanaster.allelogram.gui;
 
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Vector;
 
 import org.carlmanaster.allelogram.model.Allele;
 import org.carlmanaster.allelogram.model.CircularList;
-import org.carlmanaster.allelogram.model.Classification;
 import org.carlmanaster.allelogram.model.Classifier;
 import org.carlmanaster.allelogram.model.Genotype;
 
 public class Colorizer {
 	private final Classifier classifier;
-	private final HashMap<Vector<String>, Color> index = new HashMap<Vector<String>, Color>();
+	private final HashMap<String, Color> index = new HashMap<String, Color>();
 	
 	public Colorizer(Classifier classifier, List<Genotype> genotypes) {
 		this.classifier = classifier;
 		if (classifier == null)
 			return;
-		HashSet<Classification> set = new HashSet<Classification>();
+		HashSet<String> set = new HashSet<String>();
 		for (Genotype genotype : genotypes)
-			set.add(classifier.classify(genotype));
+			set.add(classifier.string(genotype));
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.addAll(set);
+		Collections.sort(list);
 		
 		CircularList<Color> colors = makeColors();
 		
 		int i = 0;
-		for (Vector<String> vector : set)
-			index.put(vector, colors.get(i++));
+		for (String string : list )
+			index.put(string, colors.get(i++));
 	}
 
 	private static CircularList<Color> makeColors() {
@@ -49,7 +52,7 @@ public class Colorizer {
 	public Color colorFor(Allele allele) {
 		if (classifier == null)
 			return Color.BLUE;
-		return index.get(classifier.classify(allele.getGenotype()));
+		return index.get(classifier.string(allele.getGenotype()));
 	}
 
 }
